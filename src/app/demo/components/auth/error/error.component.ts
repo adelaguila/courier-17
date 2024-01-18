497bbf3fb217764c3caf7696a53ff9c3ad843465
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-01-18 15:27:23 +0300

    fix auth font weight

--- src/app/demo/components/auth/error/error.component.ts
+++ src/app/demo/components/auth/error/error.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: './error.component.html'
+    templateUrl: './error.component.html',
+    styles: [
+        `
+            :host ::ng-deep .p-button-label {
+                font-weight: 400;
+            }
+        `,
+    ],
 })
-export class ErrorComponent {
-
-}
+export class ErrorComponent {}
